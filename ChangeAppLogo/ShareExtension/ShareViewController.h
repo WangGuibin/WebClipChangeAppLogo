6fbe76ff91e7d825853823021b37630d9475b938
Wangguibin <864562082@qq.com>
2020-11-01 16:16:00 +0800

    add share extension

---
+++ ChangeAppLogo/ShareExtension/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  ShareExtension
+//
+//  Created by 王贵彬 on 2020/11/1.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
